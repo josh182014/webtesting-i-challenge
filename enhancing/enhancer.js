0a7f8169c4eaa7789b401ff81bd9668d673c6ef9
josh182014 <josh182014@gmail.com>
2019-06-17 15:52:38 -0600

    changed file indentation

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,53 +1,53 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+    succeed,
+    fail,
+    repair,
+    get,
 };
 
 function succeed(item) {
-  if (item.enhancement === 20 ) {
-    return {...item}
-  }
-  else if (item.enhancement >=0 && item.enhancement <=20) {
-    return {...item, enhancement: item.enhancement + 1};
-  }
-  else {
-    return {error: "Check ranges"}
-  }
+    if (item.enhancement === 20) {
+        return { ...item }
+    }
+    else if (item.enhancement >= 0 && item.enhancement <= 20) {
+        return { ...item, enhancement: item.enhancement + 1 };
+    }
+    else {
+        return { error: "Check ranges" }
+    }
 }
 
 function fail(item) {
-  if (item.durability >= 0 && item.durability <= 100 && item.enhancement >= 0 && item.enhancement <= 20) {
-    if (item.enhancement < 15 ) {
-      return {...item, durability: item.durability - 5 }
+    if (item.durability >= 0 && item.durability <= 100 && item.enhancement >= 0 && item.enhancement <= 20) {
+        if (item.enhancement < 15) {
+            return { ...item, durability: item.durability - 5 }
+        }
+        else if (item.enhancement >= 15 && item.enhancement < 17) {
+            return { ...item, durability: item.durability - 10 }
+        }
+        else if (item.enhancement > 16) {
+            return { ...item, durability: item.durability - 10, enhancement: item.enhancement - 1 }
+        }
     }
-    else if (item.enhancement >= 15 && item.enhancement < 17 ) {
-      return { ...item, durability: item.durability - 10 }
+    else {
+        return { error: "Check ranges" }
     }
-    else if (item.enhancement > 16 ) {
-      return {...item, durability: item.durability - 10, enhancement: item.enhancement - 1 }
-    }
-  }
-  else {
-    return {error: "Check ranges"}
-  }
 }
 
 function repair(item) {
-  if (item.durability >= 0 && item.durability <= 100) {
-    return { ...item, durability: 100 };
-  }
-  else {
-    return {error: "Check ranges"}
-  }
+    if (item.durability >= 0 && item.durability <= 100) {
+        return { ...item, durability: 100 };
+    }
+    else {
+        return { error: "Check ranges" }
+    }
 }
 
 function get(item) {
-  if (item.enhancement > 0 ) {
-    return { ...item, name: `[+${item.enhancement}] ${item.name}`};
-  }
-  else {
-    return {...item}
-  }
+    if (item.enhancement > 0) {
+        return { ...item, name: `[+${item.enhancement}] ${item.name}` };
+    }
+    else {
+        return { ...item }
+    }
 }
