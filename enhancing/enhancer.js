8b1294e804ff361aeed5f43f89feecdac7b53944
josh182014 <josh182014@gmail.com>
2019-06-17 15:07:43 -0600

    added error checking for user input

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -9,13 +9,29 @@ function succeed(item) {
   if (item.enhancement === 20 ) {
     return item
   }
-  else {
+  else if (item.enhancement >=0 && item.enhancement <=20) {
     return {...item, enhancement: item.enhancement + 1};
+  }
+  else {
+    return {error: "Check ranges"}
   }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.durability >= 0 && item.durability <= 100 && item.enhancement >= 0 && item.enhancement <= 20) {
+    if (item.enhancement < 15 ) {
+      return {...item, durability: item.durability - 5 }
+    }
+    else if (item.enhancement >= 15 && item.enhancement < 17 ) {
+      return { ...item, durability: item.durability - 10 }
+    }
+    else if (item.enhancement > 16 ) {
+      return {...item, durability: item.durability - 10, enhancement: item.enhancement - 1 }
+    }
+  }
+  else {
+    return {error: "Check ranges"}
+  }
 }
 
 function repair(item) {
@@ -23,7 +39,7 @@ function repair(item) {
     return { ...item, durability: 100 };
   }
   else {
-    return {error: "check yourself"}
+    return {error: "Check ranges"}
   }
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -10,17 +10,32 @@ const enhancer = require('./enhancer.js');
 describe('enhancer.js', () => {
     describe('repair()', () => {
         it('restores durability to 100', () => {
-            expect(enhancer.repair({ durability: 899 })).toEqual({error: "check yourself"})
+            expect(enhancer.repair({ durability: 899 })).toEqual({error: "Check ranges"})
             expect(enhancer.repair({ durability: 20 }).durability).toBe(100)
-            expect(enhancer.repair({ durability: -20 })).toEqual({error: "check yourself"})
+            expect(enhancer.repair({ durability: -20 })).toEqual({error: "Check ranges"})
         })
     })
     
-    describe('success()', () => {
+    describe('succeed()', () => {
         it('increases enhanment by 1 to a max of 20', () => {
             expect(enhancer.succeed({enhancement: 10}).enhancement).toBe(11)
             expect(enhancer.succeed({enhancement: 20}).enhancement).toBe(20)
-            expect(enhancer.succeed({enhancement: -10}).enhancement).toBe(-9)
+            expect(enhancer.succeed({enhancement: -3})).toEqual({error: 'Check ranges'})
+        })
+    })
+
+    describe('fail()', () => {
+        it('decreases the durability by 5', () => {
+            expect(enhancer.fail({durability: 10, enhancement: 10 }).durability).toBe(5);
+        })
+        it('decreases the durability by 10', () => {
+            expect(enhancer.fail({durability: 10, enhancement: 15 }).durability).toBe(0)
+            expect(enhancer.fail({durability: 15, enhancement: 16 }).durability).toBe(5)
+        })
+        it('decreases the durability by 10 and enhancement by 1', () => {
+            expect(enhancer.fail({durability: 15, enhancement: 18 }).durability).toBe(5)
+            expect(enhancer.fail({durability: 15, enhancement: 18 }).enhancement).toBe(17)
+            expect(enhancer.fail({durability: 19, enhancement: 19 })).toEqual({durability: 9, enhancement: 18})
         })
     })
 })
